4b60c193ed347984bac427d6a09559f04f90858b
yan <yanbreno27@gmail.com>
2024-03-02 16:48:50 -0300

    C:/Program Files/Git/

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Projeto Login Responsivo
+O site se trata de um campo de pesquisa o qual o usuário preenche com o nome de uma cidade e o na tela são retornadas informações como clima, temperatura, velocidade do vento, etc. Foi utilizada a Weather API para colher as informações necessárias.
+
+## Tecnologias Envolvidas
+![CSS3](https://img.shields.io/badge/CSS3-fff?style=for-the-badge&logo=css3&logoColor=264CE4)
+![HTML5](https://img.shields.io/badge/HTML5-fff?style=for-the-badge&logo=html5)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+## Como Visualizar
+Basta clicar em [Dados do Tempo (yan2024.github.io)](https://yan2024.github.io/dados-do-tempo/) que o site abrirá num navegador da web.
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const chaveDaApi = "917cef81a7eb44b090230628232511"; 
+
+const $botaoDePesquisa = document.querySelector(".botao")
+
+$botaoDePesquisa.addEventListener("click", async () => {
+    const cidade = document.getElementById("ipesquisa").value;
+
+    document.getElementById("ipesquisa").value = "" 
+
+    const dados = await buscarDadosDaCidade(cidade); 
+    
+    if (!cidade) return;                          
+
+    if(dados) preencherDadosNaTela(dados, cidade) 
+})
+
+async function buscarDadosDaCidade(cidade) { 
+    const apiURL = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+    
+    const resposta = await fetch(apiURL); 
+    
+    if(resposta.status !== 200) return; 
+
+    const dados = resposta.json();
+
+    return dados; 
+}
+
+function preencherDadosNaTela(dados, cidade) { 
+    const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const humidade = dados.current.humidity;
+    const velocidade = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+
+    document.getElementById("cidade").textContent = cidade;
+
+    document.getElementById("temperatura").textContent = `${temperatura}°C`;
+
+    document.getElementById("condicao").textContent = condicao;
+
+    document.getElementById("humidade").textContent = `${humidade}%`;
+
+    document.getElementById("velocidade").textContent = `${velocidade} km/h`;
+
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao)
+}
